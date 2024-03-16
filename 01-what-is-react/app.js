6a0614d30d9e2d8b32c0ae731953d6ef85477e65
Clement127001 <babiyonclement18@gmail.com>
2024-03-16 17:57:44 +0530

    add elements using the jsx syntax

--- 01-what-is-react/app.js
+++ 01-what-is-react/app.js
@@ -1,41 +1,15 @@
-//how to add heading the react way
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// //<h1>Welcome to react - created by react</h1>
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "welcome to react - created by react"
-// );
+//creating the react element using JSX(html or more like xml syntax)
 
-// console.log(heading);
-
-//creating the nested structure using the react.createElement()
-/*
-<div id="parent">
-
-    <div id="child1">
-        <h1>Heading tag h1</h1>
-        <h2>Heading tag h2</h2>
-    </div>
-
-    <div id="child2">
-        <h1>Heading tag h1</h1>
-        <h2>Heading tag h2>/h2>
-    </div>
-
-</div>
-*/
-
-const nestedElement = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: "child1" }, [
-    React.createElement("h1", { key: "heading 1" }, "Heading tag h1"),
-    React.createElement("h2", { key: "heading2" }, "Heading tag h2"),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", { key: "heading1" }, "Heading tag h1"),
-    React.createElement("h2", { key: "heading2" }, "Heading tag h2"),
-  ]),
-]);
+//JSX -> transpiled by babel -> React.createElement() -> ReactElement -> render -> html element
+const heading = (
+  <h1 id="heading" className="header">
+    Welcome to react with jsx
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(nestedElement);
+
+root.render(heading);
