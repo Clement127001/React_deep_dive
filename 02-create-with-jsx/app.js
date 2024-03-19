0a59d54793d0e1f85e8fd2652b037135be1c3c08
Clement127001 <babiyonclement18@gmail.com>
2024-03-19 13:58:28 +0530

    use component to create the basic UI

--- 02-create-with-jsx/app.js
+++ 02-create-with-jsx/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//using the variable to hold the element -> need to evaluated using the expression => {}
 //adding it using the jsx syntax
 const heading = React.createElement(
   "h1",
@@ -8,14 +9,36 @@ const heading = React.createElement(
   "This is the heading using react element"
 );
 
-//using functional component
-const Header = () => (
+//using the jsx to create the simple header
+
+const heading2 = <h1>This is heading using jsx</h1>;
+
+const Heading2 = () => <h1>This is heading using jsx</h1>;
+
+const heading3 = React.createElement("div", {}, [heading, <Heading2 />]);
+
+//using component to build header
+const Heading = () => (
+  <header>
+    <h1>Heading 2</h1>
+    <p>
+      para <span>1</span>
+    </p>
+  </header>
+);
+//using functional component -> embedding the component within another component is called compound component
+const Home = () => (
   <div>
-    {heading}
+    {/* {heading}
+    {heading2}
+    <Heading /> */}
+    {heading3}
     <p>This is the paragraph</p>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(<Header></Header>);
+//ways to render a component
+// root.render(<Home></Home>); =>while we are having the children within component
+root.render(<Home />);
