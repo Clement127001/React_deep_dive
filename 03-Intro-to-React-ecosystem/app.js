41a53ac4796301fcf2b1073eef0067e306b9fe61
Clement127001 <babiyonclement18@gmail.com>
2024-03-19 22:55:42 +0530

    add the basic setup

---
+++ 03-Intro-to-React-ecosystem/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = <h1>This is the heading element</h1>;
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(heading);
