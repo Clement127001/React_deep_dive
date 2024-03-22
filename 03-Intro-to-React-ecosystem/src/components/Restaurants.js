f6a555ec77a41cc6f0846c1737f8b75d615808e0
Clement127001 <babiyonclement18@gmail.com>
2024-03-22 21:20:45 +0530

    fetch data from the real time API

--- 03-Intro-to-React-ecosystem/src/components/Restaurants.js
+++ 03-Intro-to-React-ecosystem/src/components/Restaurants.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import data from "../utils/data.js";
 import RestaurantCard from "./RestaurantCard.js";
 
@@ -7,13 +7,32 @@ export default Restaurants = () => {
   // const arr = useState(data);
   // const [restaurantList, setRestaurantList] = arr;
 
-  const [restaurantList, setRestaurantList] = useState(data);
+  const [restaurantList, setRestaurantList] = useState([]);
 
   function filterRestaurants() {
-    const newRestaurants = restaurantList.filter((res) => res.avgRating >= 4);
+    const newRestaurants = restaurantList.filter(
+      (res) => res.info.avgRating >= 4.5
+    );
 
     setRestaurantList(newRestaurants);
   }
+
+  const fetchResturants = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const data = await response.json();
+    setRestaurantList(
+      data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  useEffect(() => {
+    fetchResturants();
+  }, []);
+
+  restaurantList?.map((res) => console.log(res.info.id));
+
   return (
     <div className="restaurants-list">
       <section>
@@ -25,8 +44,11 @@ export default Restaurants = () => {
       <section className="restaurant-cards">
         <h2 className="restaurants-title">Restaurants that you will like</h2>
         <div className="card-container">
-          {restaurantList.map((restaurant) => (
-            <RestaurantCard key={restaurant.id} resData={restaurant} />
+          {restaurantList?.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant.info.id}
+              resData={restaurant.info}
+            />
           ))}
         </div>
       </section>
