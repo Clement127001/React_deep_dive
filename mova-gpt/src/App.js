6e52f78e527b7d24d7f89b305c32bef1016414f3
Clement127001 <babiyonclement18@gmail.com>
2024-04-19 23:17:40 +0530

    initial setup

---
+++ mova-gpt/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-red-600 text-3xl">
+        This is the new application - MOVA-GPT
+      </h1>
+    </div>
+  );
+}
+
+export default App;
